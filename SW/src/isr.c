94dc2a1c5c5d56345fb85bc09c5a10d78ed90789
cracked-machine <chrisjsutton@gmail.com>
2023-04-09 17:19:50 +0000

    moved system isr to separate c file

---
+++ SW/src/isr.c
@@ -0,0 +1,35 @@
+#include "stm32l011xx.h"
+
+
+
+void NMI_Handler(void)
+{
+    while (1)
+    {
+        // trouble
+    }
+}
+void HardFault_Handler(void)
+{
+    while (1)
+    {
+        // trouble
+    }
+}
+void SVC_Handler(void)
+{
+    while (1)
+    {
+        // trouble
+    }
+}
+void PendSV_Handler(void)
+{
+    while (1)
+    {
+        // trouble
+    }
+}
+void SysTick_Handler(void)
+{
+}
