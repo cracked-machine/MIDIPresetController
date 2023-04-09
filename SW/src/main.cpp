06f4b955781a6970b7e2bffff300a7ea9ca89460
cracked-machine <chrisjsutton@gmail.com>
2023-04-09 14:49:45 +0000

    refactored led code

--- SW/src/main.cpp
+++ SW/src/main.cpp
@@ -8,25 +8,37 @@ static inline void spin(volatile uint32_t count)
         asm("nop");
 }
 
-int main(void)
+void setup_led(void)
 {
     // enable GPIO RCC
     RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
-
     // config PA7 = output, pushpull, highspeed, no PU/PD
     GPIOA->MODER |= (GPIO_MODER_MODE7_0);
     GPIOA->MODER &= ~(GPIO_MODER_MODE7_1);
     GPIOA->OTYPER &= ~(GPIO_OTYPER_OT_7);
     GPIOA->OSPEEDR |= (GPIO_OSPEEDER_OSPEED7_Msk);
     GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD7_Msk);
+}
+
+void toggle_led(bool enable, uint32_t sleep=0)
+{
+    if (enable)
+        GPIOA->BSRR |= GPIO_BSRR_BS_7;
+    else
+        GPIOA->BSRR |= GPIO_BSRR_BR_7;
+    spin(sleep);
+}
+
+int main(void)
+{
+    setup_led();
+
 
     while (1)
     {
         // blinky!
-        GPIOA->BSRR |= GPIO_BSRR_BS_7;
-        spin(99999);
-        GPIOA->BSRR |= GPIO_BSRR_BR_7;
-        spin(99999);
+        toggle_led(true, 99999);
+        toggle_led(false, 99999);
     }
 }
 
