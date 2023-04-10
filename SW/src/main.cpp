30331101e85cbb2034d3ce5153d93e5bf2aa5ae6
cracked-machine <chrisjsutton@gmail.com>
2023-04-10 11:21:22 +0000

    tidied up comments

--- SW/src/main.cpp
+++ SW/src/main.cpp
@@ -3,71 +3,57 @@
 #include "stm32l011xx.h"
 
 
-
 static inline void spin(volatile uint32_t count)
 {
     while (count--)
         asm("nop");
 }
 
-void setup_led()
+void setup()
 {
-
-    // config PA7 as output (0x01)
-    GPIOA->MODER |= (GPIO_MODER_MODE7_0);
+    RCC->IOPENR |= RCC_IOPENR_GPIOAEN;                                  // enable GPIO clock (used by multiple porta pins)
+    
+    // LED //
+    /////////
+    GPIOA->MODER |= (GPIO_MODER_MODE7_0);                               // config PA7 as output (0x01)
     GPIOA->MODER &= ~(GPIO_MODER_MODE7_1);
-    // setup p7 as pushpull, highspeed, no PU/PD
-    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT_7);
+    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT_7);                               // setup p7 as pushpull, highspeed, no PU/PD
     GPIOA->OSPEEDR |= (GPIO_OSPEEDER_OSPEED7_Msk);
-    GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD7_Msk);
-}
+    GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD7_Msk);    
 
-void setup_exti()
-{
-    // mux external interrupts line for pa10 (syscfg_exticr3) to the internal edge detector 
-    // SYSCFG->EXTICR[2] |= (SYSCFG_EXTICR3_EXTI10_PA);
-
+    // EXTI //
+    //////////
+    // SYSCFG->EXTICR[2] |= (SYSCFG_EXTICR3_EXTI10_PA);                 // mux external interrupts line for pa10 (syscfg_exticr3) 
+                                                                        // to the internal edge detector 
     // PA10
-    // setup pa10 gpio as input (0x00)
-    GPIOA->MODER &= ~((GPIO_MODER_MODE10_0) | (GPIO_MODER_MODE10_1));
-    // set pa10 gpio pullup (0x01)
-    GPIOA->PUPDR |= (GPIO_PUPDR_PUPD10_0);
+    GPIOA->MODER &= ~((GPIO_MODER_MODE10_0) | (GPIO_MODER_MODE10_1));   // setup pa10 gpio as input (0x00)
+    GPIOA->PUPDR |= (GPIO_PUPDR_PUPD10_0);                              // set pa10 gpio pullup (0x01)
     GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD10_1);
-
-    // enable interupt request in interrupt mask register
-    EXTI->IMR |= EXTI_IMR_IM10_Msk;
-    // program trigger resistors with edge detection
-    EXTI->FTSR |= EXTI_FTSR_FT10_Msk;
+    EXTI->IMR |= EXTI_IMR_IM10_Msk;                                     // enable interupt request in interrupt mask register
+    EXTI->FTSR |= EXTI_FTSR_FT10_Msk;                                   // program trigger resistors with edge detection
 
     // PA9 
-    // setup pa10 gpio as input (0x00)
-    GPIOA->MODER &= ~((GPIO_MODER_MODE9_0) | (GPIO_MODER_MODE9_1));
-    // set pa10 gpio pullup (0x01)
-    GPIOA->PUPDR |= (GPIO_PUPDR_PUPD9_0);
+    GPIOA->MODER &= ~((GPIO_MODER_MODE9_0) | (GPIO_MODER_MODE9_1));     // setup pa10 gpio as input (0x00)
+    GPIOA->PUPDR |= (GPIO_PUPDR_PUPD9_0);                               // set pa10 gpio pullup (0x01)
     GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD9_1);
-
-    // enable interupt request in interrupt mask register
-    EXTI->IMR |= EXTI_IMR_IM9_Msk;
-    // program trigger resistors with edge detection
-    EXTI->FTSR |= EXTI_FTSR_FT9_Msk;
-
-    // ENABLE EXTI9/EXTI10
-    // configure nvic irq channel and prio
-    NVIC_EnableIRQ(EXTI4_15_IRQn); 
+    EXTI->IMR |= EXTI_IMR_IM9_Msk;                                      // enable interupt request in interrupt mask register
+    EXTI->FTSR |= EXTI_FTSR_FT9_Msk;                                    // program trigger resistors with edge detection
+    
+    NVIC_EnableIRQ(EXTI4_15_IRQn);                                      // configure nvic irq channel and prio
     NVIC_SetPriority(EXTI4_15_IRQn,0); 
-}
 
-void setup_timer()
-{
-    RCC->APB2ENR |= RCC_APB2ENR_TIM21EN_Msk;
-    TIM21->CR1 |= (TIM_CR1_ARPE_Msk);       // preload buffer
-    TIM21->DIER |= (TIM_DIER_UIE_Msk);      // enable interrupts
-    TIM21->PSC = 32;                         // prescaler
-    TIM21->ARR = 0xFFFFFFFF;                      // auto-reload value
+
+    // TIMER21 //
+    /////////////
+    RCC->APB2ENR |= RCC_APB2ENR_TIM21EN_Msk;                            // enable the APB2 clock used by TIM21
+    TIM21->CR1 |= (TIM_CR1_ARPE_Msk);                                   // preload buffer
+    TIM21->DIER |= (TIM_DIER_UIE_Msk);                                  // enable interrupts
+    TIM21->PSC = 32;                                                    // prescaler
+    TIM21->ARR = 0xFFFFFFFF;                                            // auto-reload value
     NVIC_EnableIRQ(TIM21_IRQn); 
     NVIC_SetPriority(TIM21_IRQn,0); 
-    // start the timer
-    TIM21->CR1 |= (TIM_CR1_CEN_Msk);
+    TIM21->CR1 |= (TIM_CR1_CEN_Msk);                                    // start the timer
+
 }
 
 void enable_led(bool enable, uint32_t sleep = 0)
@@ -79,21 +65,19 @@ void enable_led(bool enable, uint32_t sleep = 0)
     spin(sleep);
 }
 
-void toggle_led(uint32_t sleep=0)
+void toggle_led(uint32_t sleep = 0)
 {
     if (GPIOA->ODR & GPIO_ODR_OD7_Msk)
         GPIOA->BSRR |= GPIO_BSRR_BR_7;
     else
         GPIOA->BSRR |= GPIO_BSRR_BS_7;
+    spin(sleep);
 }
 
 int main()
 {
-    // enable GPIO RCC
-    RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
-    setup_led();
-    setup_exti();
-    setup_timer();
+
+    setup();
 
     while (1)
     {
@@ -118,6 +102,7 @@ void TIM21_IRQHandler()
 {
     // do stuff
     toggle_led();
+
     // clear the interrupt
     TIM21->SR &= ~(TIM_SR_UIF_Msk);
 }
